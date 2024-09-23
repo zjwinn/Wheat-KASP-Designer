9ba7ee1b75950012b6e1d2168984d26b6256652e
Zachary Winn <zwinn@outlook.com>
2024-09-23 13:28:32 -0500

    Resturcture test scripts

---
+++ test_files/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Run test file 
+bash ./Wheat-KASP-Designer.sh \
+    --input-file 'NC13-20076xGA06493-13LE6_filt_fixed.vcf.gz' \
+    --reference-geno  '/project/90daydata/gbru_wheat2/zjwinn_project_directory/Ref/161010_Chinese_Spring_v1.0_pseudomolecules.fasta' \
+    --output-file 'test.txt' \
+    --snps 'test_snps.txt' \
+    --verbose \
+    --debug
