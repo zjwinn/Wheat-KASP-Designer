b1cf0e1dbf6d2cbd0043b62fefa33be8f29a9e64
Zachary Winn <zwinn@outlook.com>
2024-10-03 11:25:21 -0500

    Update test.sh

--- test_files/test.sh
+++ test_files/test.sh
@@ -37,7 +37,7 @@ else
         --input-file 'NC13-20076xGA06493-13LE6_filt_fixed.vcf.gz' \
         --reference-geno  '/project/90daydata/gbru_wheat2/zjwinn_project_directory/Ref/161010_Chinese_Spring_v1.0_pseudomolecules.fasta' \
         --snps 'test_snps.txt' \
-        --debug--unique-id 'CHS10' \
+        --unique-id 'CHS10' \
         --verbose \
         --kasp \
         --debug
